1efe5fdbff06c6f06cc994fbfe8ac0664a9ae6e3
Ruchag0803 <ruchagavas2003@gmail.com>
2024-04-01 08:15:22 +0530

    Files uploaded to main

---
+++ E_commerce_Application/app/build/generated/data_binding_trigger/debug/com/example/e_commerce_application/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.example.e_commerce_application;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
---
+++ E_commerce_Application/app/build/tmp/compileDebugJavaWithJavac/compileTransaction/stash-dir/BR.java.uniqueId11
@@ -0,0 +1,5 @@
+package com.example.e_commerce_application;
+
+public class BR {
+  public static final int _all = 0;
+}
